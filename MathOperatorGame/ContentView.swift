0af4847fe15dd56d5074c0194a3783611a69f942
Ángel Ponce <angelillopr98@icloud.com>
2026-06-24 17:46:56 +0200

    Initial Commit

---
+++ MathOperatorGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MathOperatorGame
+//
+//  Created by Ángel Ponce Reguera on 24/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MathOperatorGame/MathOperatorGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathOperatorGameApp.swift
+//  MathOperatorGame
+//
+//  Created by Ángel Ponce Reguera on 24/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MathOperatorGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
